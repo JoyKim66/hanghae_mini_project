42846330b2efec8e2aa392fb9134024d7cabace6
JoyKim66 <masscom6051@naver.com>
2022-06-11 13:49:18 +0900

    게시글 상세페이지 뷰 구현 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
+import  "./App.css"
 import React from "react";
-import Write from "./Write";
-import Main from "./Main";
-import Detail from "./Detail";
 
 
 function App() {
   return (
     <div className="App">
-
     </div>
   );
 }
--- src/Detail.js
+++ src/Detail.js
@@ -1,27 +1,86 @@
-import React from "react";
-
+import * as React from 'react';
 import styled from "styled-components";
+
 
 
 const Detail = () => {
     return (
-        <div>
-            콘테이너부분
-            <div>이미지부분</div>
-            <div>
-                텍스트부분
-                <div>카페이름</div>
-                <div>카테고리</div>
-                <div>원두이름</div>
-                <div>
-                    리뷰내용
-                    <input type="text"/>
-                    <button>수정</button>
-                    <button>삭제</button>
-                </div>
-            </div>
-        </div>
+        <Container>
+            <ImageBox>이미지부분</ImageBox>
+            <TextBox>
+                <NameBox>카페이름
+                <div><hr style={{width:"100%"}}/></div>
+                </NameBox>
+                <CategoryBox>원두이름:</CategoryBox>
+                <ReviewBox>
+                    <div>
+                        <Review>
+                            리뷰내용
+                        </Review>
+                        <ButtonBox>
+                            <Button>수정</Button>
+                            <Button>삭제</Button>
+                        </ButtonBox>
+                        
+                    </div>
+                </ReviewBox>
+            </TextBox>
+        </Container>
     )
 }
 
 export default Detail;
+
+const Container = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+`;
+const ImageBox = styled.div`
+    width: 50%;
+    height: 500px;
+    border: 1px solid #d3d3d3;  
+    margin: 20px;  
+`;  
+const TextBox = styled.div`
+    width: 50%;
+    height: 500px;
+    border: 1px solid #d3d3d3;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    padding: 0 10px;
+    margin: 20px;
+    `;
+const NameBox = styled.div`
+    width: 100%;
+    height: 100px;
+    font-size: large;
+    padding: 20px 0;
+    font-weight: 700;
+`;
+const CategoryBox = styled.div`
+    width: 100%;
+    height: 100px;`;
+
+const ReviewBox = styled.div`
+    display: flex;
+    flex-direction: column;    
+`;
+const Review = styled.div`
+    width: 100%;
+    height: 200px;
+    border: 1px solid #d3d3d3;
+`;
+const ButtonBox = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    margin: 10px 0;
+`;
+const Button = styled.button`
+    width: 100px;
+    height: 30px;
+    background: #848484;
+    border: 1px solid #fff;
+    border-radius: 3px;
+    color: #fff;
+`;
